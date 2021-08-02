8665bce869e20ff65dbf7ad852cbd1ea710703bf
TomAndersen0714 <1040994898@qq.com>
2021-08-02 10:33:08 +0800

    update xqc v2

--- xqc_across_platform_utils/conf/xqc_msg_header_demo.py
+++ xqc_across_platform_utils/conf/xqc_msg_header_demo.py
@@ -1,6 +1,12 @@
+import uuid
+
+batch_id = str(uuid.uuid4())
 header: dict[str:str] = {
-    "task_id": "task_id",
+    'task_id': 'test',
     "db_type": "clickhouse",
-    "target_table": "buffer.test_buffer",
-    "partition": "{{ds_nodash}}"
+    "target_table": 'tmp.drop_partition_test_all',
+    "clear_table": 'tmp.drop_partition_test_local',
+    "partition": "(2021,7,13)",
+    "cluster_name": "cluster_3s_2r",
+    "batch_id": batch_id
 }
