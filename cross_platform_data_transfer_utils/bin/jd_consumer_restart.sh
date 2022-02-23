7d5ed67b994bca34887eb3736fbcf2f950137699
chengcheng <chengcheng@xiaoduotech.com>
2022-02-23 10:57:49 +0800

    update

--- cross_platform_data_transfer_utils/bin/jd_consumer_restart.sh
+++ cross_platform_data_transfer_utils/bin/jd_consumer_restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # kill old program
-ps -ef | grep 'python3 cross_platform_pulsar_consumer' | sed -n '1p' | awk '{print $2}' | xargs kill
+ps -ef | grep 'python3 cross_platform_pulsar_consumer' | grep -v 'grep' | sed -n '1p' | awk '{print $2}' | xargs kill
+# sleep for closing
+sleep 10s
 # initiate a new one
 nohup python3 ../src/cross_platform_pulsar_consumer_v3.py ../conf/tb_to_jd.json 1>/dev/null 2>&1 &
--- cross_platform_data_transfer_utils/bin/tb_consumer_restart.sh
+++ cross_platform_data_transfer_utils/bin/tb_consumer_restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # kill old one
-ps -ef | grep 'cross_platform_pulsar_consumer_v3.py' | sed -n '1p' | awk '{print $2}' | xargs kill
+ps -ef | grep 'cross_platform_pulsar_consumer_v3.py' | grep -v 'grep' | sed -n '1p' | awk '{print $2}' | xargs kill
+# sleep for closing
+sleep 10s
 # initiate a new one
 nohup python3 ../src/cross_platform_pulsar_consumer_v3.py ../conf/all_platform_to_tb.json 1>/dev/null 2>&1 &
